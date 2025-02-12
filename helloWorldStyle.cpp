9a32546663771f16c3ea61c08b1b3d77349c63d3
Mikhail Ibrahim <Mikhail.ibrahim@stu.ocsb.ca>
2025-02-12 03:11:39 +0000

    Added Hello World with Style program

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,10 @@
+// Copyright 2024 Mikhail Ibrahim
+// This program prints "Hello, World with Style!" with proper formatting.
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World with Style!" << std::endl;
+    std::cout << "Learning C++ is fun!" << std::endl;
+    return 0;
+}
